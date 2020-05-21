2ff6d572802f768df8198be45f4eb5a4d4f1514a
ginabomination <64622072+ginabomination@users.noreply.github.com>
2020-05-22 09:00:48 +1200

    fixing while mistake

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
-var userInput = document.querySelector("#userInput");
-var submit = document.querySelector("#submit");
-var botChat = document.querySelector("#botChat");
-var user = document.querySelector("#user");
+const userInput = document.querySelector("#userInput");
+const submit = document.querySelector("#submit");
+const botChat = document.querySelector("#botChat");
+const user = document.querySelector("#user");
 
 alert ("testing")
 
@@ -12,13 +12,8 @@ greetingList = ["Hello", "Hi", "Hey there", "Yo"];
 greeting = Math.floor(Math.random() * greetingList.length);
 
 function response() {
-    while(botChat.textContent == "What is your name?") {
-        user.style.color = "red";
-        botChat.innerHTML = greetingList[greeting] + " " + userInput.value;
-        user.innerHTML = userInput.value;
-        setTimeout(() => {botChat.innerHTML = "What is your favourite colour?" }, 2000);;
-        userInput.value = null;
     } 
+    
     while (botChat.extContent == "What is your name?") {
         setTimeout(() => {botChat.innerHtml = "What is your favourite colour?
         userInput.value = user.style.colour
@@ -32,6 +27,4 @@ function response() {
             botChat.textContent = "Please enter a simple colour. Im not an expert in my colours.";
             setTimeout (() => {botChat.innerHtml = "What is your favourite colour?"}, 2000;;
             userInput.value = null;
-        }
-    }
-}
+                        }}}}
